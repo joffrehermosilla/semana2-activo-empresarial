c10a0b94d6f32b9ffa5461b7f07ba3ba4f9bcd2f
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 00:22:02 -0500

    primer commit

---
+++ microservicio-activo-empresarial/src/main/java/nttdata/bootcamp/microservicios/activo/empresarial/MicroservicioActivoEmpresarialApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.activo.empresarial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioActivoEmpresarialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioActivoEmpresarialApplication.class, args);
+	}
+
+}
---
+++ microservicio-activo-empresarial/src/test/java/nttdata/bootcamp/microservicios/activo/empresarial/MicroservicioActivoEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.activo.empresarial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioActivoEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
